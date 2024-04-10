8858612015e9ca393a47b5e147b1a24588eb6b49
*Lemi-in* <*lemesaelias@gmail.com*>
2024-04-10 14:57:29 +0300

    build the articles and model schema

--- Blog-article/server.js
+++ Blog-article/server.js
@@ -1,29 +1,21 @@
-const express = require('express');
-const app = express();
-const articlesRouter = require('./routes/articles');
-const mongoose = require('mongoose');
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
 
 mongoose.connect('mongodb://localhost/blog');
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-app.use('/articles', articlesRouter)
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Article 1',
-        createdAt: new Date(),
-        description: 'Description of article 1'
-    }, {
-        title: 'Article 2',
-        createdAt: new Date(),
-        description: 'Description of article 2'
+app.use('/articles', articleRouter)
 
-    }]
-
-    res.render('articles/index' ,{ articles: articles});
-});
-
-app.listen(5000, () => {
-  console.log('server started');
-});
+app.listen(5000)
